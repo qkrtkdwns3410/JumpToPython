865cec10c896e11fcbc67db25bc31bf60771328a
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-03-17 00:05:06 +0900

    파이썬 알고리즘 시작

--- JumpToPython/파이썬둘러보기/백준_그리디/1343_폴리오미노_S5.py
+++ JumpToPython/파이썬둘러보기/백준_그리디/1343_폴리오미노_S5.py
@@ -9,3 +9,34 @@
  * -----------------------------------------------------------
  * 2022-03-16        ipeac       최초 생성
  """
+
+
+def Solution(ex1):
+      print("ex1 : %s " % ex1)
+      ex1 = ex1.rstrip()
+      polio_a = "AAAA"
+      polio_b = "BB"
+
+      if 'XXXX' in ex1:
+            ex1 = ex1.replace('XXXX', polio_a)
+      if 'XX' in ex1:
+            ex1 = ex1.replace('XX', polio_b)
+
+      if 'X' in ex1:
+            print(-1)
+      else:
+            print(ex1)
+
+
+a = input()
+Solution(a)
+
+Solution("XXXXXX")
+print("==========================================")
+Solution("XX.XX")
+print("==========================================")
+Solution("XXXX....XXX.....XX")
+print("==========================================")
+Solution('X')
+print("==========================================")
+Solution("XX.XXXXXXXXXX..XXXXXXXX...XXXXXX")
