45cd4275fa85e9fcd9dc5c53ae1d78bdda609c5e
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-03-17 00:25:21 +0900

    파이썬 알고리즘 시작

--- JumpToPython/파이썬둘러보기/백준_그리디/1789_수들의_합_S5.py
+++ JumpToPython/파이썬둘러보기/백준_그리디/1789_수들의_합_S5.py
@@ -12,9 +12,28 @@
 
 
 def Solution(ex1):
+      print("ex1 : %s " % ex1)
+      ex1 = int(ex1)
+
+      init_num = 1
+      count_result = 0
+
+      while ex1 >= 0:
+            if ex1 - init_num < 0:
+                  break
+
+            else:
+                  ex1 -= init_num
+
+            init_num += 1
+            count_result += 1
+            print("ex1 : %s " % ex1)
+
+      print(count_result)
 
 
-
+a = input()
+Solution(a)
 
 Solution("200")
 print("==========================================")
