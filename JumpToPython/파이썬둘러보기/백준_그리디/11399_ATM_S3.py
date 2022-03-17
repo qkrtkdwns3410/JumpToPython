f4029f40b15af566f4ca454b3e031a0852907451
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-03-17 17:08:18 +0900

    2022/03/15

--- JumpToPython/파이썬둘러보기/백준_그리디/11399_ATM_S3.py
+++ JumpToPython/파이썬둘러보기/백준_그리디/11399_ATM_S3.py
@@ -9,3 +9,28 @@
  * -----------------------------------------------------------
  * 2022-03-17        qkrtk       최초 생성
  """
+
+
+def Solution(peple_time_list):
+      peple_time_list = map(int, peple_time_list)
+      peple_time_list = sorted(peple_time_list)
+
+      print("peple_time_list : %s " % peple_time_list)
+      result_sum = 0
+      result = 0
+      for num in peple_time_list:
+            result_sum += num
+            result += result_sum
+      print(result)
+
+
+
+
+
+
+
+
+
+N = 5
+para_list = list("3 1 4 3 2".split())
+Solution(para_list)
