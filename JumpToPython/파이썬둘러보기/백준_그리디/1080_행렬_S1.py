0ed0ed7b3f3955a83f5317dd397d3f5f9fe25f64
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-03-18 17:58:57 +0900

    2022/03/15

--- JumpToPython/파이썬둘러보기/백준_그리디/1080_행렬_S1.py
+++ JumpToPython/파이썬둘러보기/백준_그리디/1080_행렬_S1.py
@@ -12,12 +12,34 @@
 
 
 def Solution(arr_two_list_init, arr_two_list_object):
+      count = 0
       print("arr_two_list_init : %s " % arr_two_list_init)
       print("arr_two_list_object : %s " % arr_two_list_object)
-      
+
+      for i in range(N - 2):
+            for j in range(M - 2):
+                  if arr_two_list_init[i][j] != arr_two_list_object[i][j]:
+                        reverse(i, j)
+                        count += 1
+      if check():
+            print(count)
+      else:
+            print("-1")
 
 
+def reverse(x, y):
+      for i in range(x, x + 3):
+            for j in range(y, y + 3):
+                  arr_two_list_init[i][j] = 1 - arr_two_list_init[i][j]
 
+
+def check():
+      for i in range(N):
+            for j in range(M):
+                  if arr_two_list_init[i][j] != arr_two_list_object[i][j]:
+                        return False
+
+      return True
 
 
 
@@ -25,15 +47,11 @@ def Solution(arr_two_list_init, arr_two_list_object):
 
 
 N, M = map(int, input().split())
-arr_two_list_init = []
 arr_two_list_object = []
+arr_two_list_init = [list(map(int, list(input()))) for _ in range(N)]
 
 for _ in range(N):
-      a = input()
-      arr_two_list_init.append(a)
-
-for _ in range(N):
-      a = input()
+      a = map(int, input().split())
       arr_two_list_object.append(a)
 
 Solution(arr_two_list_init, arr_two_list_object)
