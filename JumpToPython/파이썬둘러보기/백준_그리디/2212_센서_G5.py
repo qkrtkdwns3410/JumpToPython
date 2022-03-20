d1504892bb5cdb6ebe1714097ce42058bfa3616d
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-03-20 20:13:52 +0900

    2022/03/20

--- JumpToPython/파이썬둘러보기/백준_그리디/2212_센서_G5.py
+++ JumpToPython/파이썬둘러보기/백준_그리디/2212_센서_G5.py
@@ -9,13 +9,34 @@
  * -----------------------------------------------------------
  * 2022-03-20        qkrtk       최초 생성
  """
+import sys
 
 
-def Solution(ex1):
-      pass
+def Solution(list_x):
+      list_cha = []
+      if k > n:
+            print(0)
+            return
+      
+      for index in range(n - 1):
+            list_cha.append(list_x[index + 1] - list_x[index])
+      list_cha.sort(reverse=True)
+      for _ in range(k - 1):
+            list_cha.pop(0)
+      print(sum(list_cha))
+
+
+
+
+
 
 
 n = int(input())
 k = int(input())
+list_n = []
+list_n.extend(input().split(" "))
+list_n = list(map(int, list_n))
 
-Solution()
+list_n.sort()
+
+Solution(list_n)
