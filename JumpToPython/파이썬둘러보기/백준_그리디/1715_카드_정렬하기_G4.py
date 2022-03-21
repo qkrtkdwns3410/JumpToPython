fc664e2206bb103a3b71d73b0a87b5b6fbcef2ec
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-03-22 01:20:45 +0900

    p

--- JumpToPython/파이썬둘러보기/백준_그리디/1715_카드_정렬하기_G4.py
+++ JumpToPython/파이썬둘러보기/백준_그리디/1715_카드_정렬하기_G4.py
@@ -9,3 +9,45 @@
  * -----------------------------------------------------------
  * 2022-03-21        qkrtk       최초 생성
  """
+import heapq
+import sys
+
+n = int(sys.stdin.readline())
+cards = []
+for _ in range(n):
+      heapq.heappush(cards, int(sys.stdin.readline()))
+
+print("heapq : %s " % heapq)
+
+if len(cards) == 1:
+      print(0)
+else:
+      result = 0
+      while len(cards) > 1:
+            print("==========================================")
+            print("cards : %s " % cards)
+            
+            min_value = heapq.heappop(cards)
+            print("min_value : %s " % min_value)
+            
+            min_value_next = heapq.heappop(cards)
+            print("min_value_next : %s " % min_value_next)
+            
+            result += min_value + min_value_next
+            print("result : %s " % result)
+            heapq.heappush(cards, min_value + min_value_next)
+            print("cards : %s " % cards)
+      print(result)
+
+
+
+
+
+
+
+
+
+# Solution(3, [10, 20, 40])
+# 100
+# Solution(5, [10, 20, 40, 30, 20])
+# #
