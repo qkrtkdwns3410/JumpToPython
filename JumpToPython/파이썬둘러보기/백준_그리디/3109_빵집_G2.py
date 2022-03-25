bece9e9c47585e8a84287f82a874c2b64a39ee35
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-03-25 17:27:38 +0900

    p

---
+++ JumpToPython/파이썬둘러보기/백준_그리디/3109_빵집_G2.py
@@ -0,0 +1,19 @@
+"""
+ *packageName    : 
+ * fileName       : 3109_빵집_G2
+ * author         : qkrtk
+ * date           : 2022-03-24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-03-24        qkrtk       최초 생성
+ """
+import sys
+
+
+r, c = map(int, sys.stdin.readline().split())
+maps = []
+for y in range(r):
+      arr = list(sys.stdin.readline().replace("\n", ""))
+      maps
