6321db9757b35033327969f5fb6365df1988ef3f
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-04-07 18:28:28 +0900

    p

---
+++ JumpToPython/파이썬둘러보기/백준_그래프탐색/11724_연결요소의개수_S2.py
@@ -0,0 +1,42 @@
+"""
+ *packageName    : 
+ * fileName       : 11724_연결요소의개수_S2
+ * author         : qkrtk
+ * date           : 2022-04-07
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-04-07        qkrtk       최초 생성
+ """
+import sys
+
+# dfs 의 경우에는 최대 재귀 값을 더 열어주어야 합니다,.
+sys.setrecursionlimit(10 ** 6)
+
+# n 정점 m 간선의 개수
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+result = 0
+
+for _ in range(m):
+      u, v = map(int, input().split())
+      graph[u].append(v)
+      graph[v].append(u)
+
+print("graph : %s " % graph)
+
+
+def dfs(v):
+      visited[v] = True
+      for i in graph[v]:
+            if not visited[i]:
+                  dfs(i)
+
+
+for i in range(1, n + 1):
+      if not visited[i]:
+            dfs(i)
+            result += 1
+print(result)
--- JumpToPython/파이썬둘러보기/백준_그래프탐색/4963_섬의_개수_S2.py
+++ JumpToPython/파이썬둘러보기/백준_그래프탐색/4963_섬의_개수_S2.py
@@ -11,7 +11,7 @@
  """
 import sys
 
-sys.setrecursionlimit(10 * 6)
+sys.setrecursionlimit(10 ** 6)
 
 
 # DFS
@@ -19,10 +19,40 @@ sys.setrecursionlimit(10 * 6)
 def dfs(x, y):
       if x >= w or x < 0 or y >= h or y < 0 or graph[y][x] == 0:
             return False
-      pass
+      
+      graph[y][x] = 0
+      visited[y][x] = 1
+      
+      dfs(x + 1, y)
+      dfs(x - 1, y)
+      dfs(x - 1, y + 1)
+      dfs(x + 1, y + 1)
+      dfs(x + 1, y - 1)
+      dfs(x - 1, y - 1)
+      dfs(x, y + 1)
+      dfs(x, y - 1)
+
 
 
 
 
 while True:
       w, h = map(int, input().split())
+      if w == 0 or h == 0:
+            sys.exit(0)
+      graph = []
+      for _ in range(h):
+            width = [list(map(int, input().split()))]
+            graph.extend(width)
+      
+      cnt = 0
+      
+      visited = [[0] * w for _ in range(h)]
+      
+      for i in range(h):
+            for j in range(w):
+                  
+                  if visited[i][j] == 0 and graph[i][j] == 1:
+                        dfs(j, i)
+                        cnt += 1
+      print(cnt)
