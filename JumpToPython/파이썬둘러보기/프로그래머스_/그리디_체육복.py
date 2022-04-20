2a9a1fe9ad63f6019c6fb60584e8d1ccee7845c8
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-04-21 00:26:57 +0900

    파이썬 알고리즘 시작

---
+++ JumpToPython/파이썬둘러보기/프로그래머스_/그리디_체육복.py
@@ -0,0 +1,27 @@
+"""
+ *packageName    : 
+ * fileName       : 그리디_체육복
+ * author         : ipeac
+ * date           : 2022-04-21
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-04-21        ipeac       최초 생성
+ """
+
+
+def solution(n, lost, reserve):
+      print("==========================================")
+      print("n : %s " % n)
+      print("lost : %s " % lost)
+      print("reserve : %s " % reserve)
+      
+      answer = 0
+      return answer
+
+
+
+solution(5, [2, 4], [1, 3, 5])
+solution(5, [2, 4], [3])
+solution(3, [3], [1])
