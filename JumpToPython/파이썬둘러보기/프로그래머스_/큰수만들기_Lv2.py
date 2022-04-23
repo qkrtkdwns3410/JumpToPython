77646f3834225a5141c805762254ce2e02b2fde5
jihye94 <qkrtkdwns3410@naver.com>
2022-04-23 18:46:29 +0900

    p

--- JumpToPython/파이썬둘러보기/프로그래머스_/큰수만들기_Lv2.py
+++ JumpToPython/파이썬둘러보기/프로그래머스_/큰수만들기_Lv2.py
@@ -9,55 +9,33 @@
  * -----------------------------------------------------------
  * 2022-04-23        jihye94       최초 생성
  """
-import collections
 
 
 def solution(number, k):
       print("==========================================")
       print("number : %s " % number)
       print("k : %s " % k)
-      res = []
-      cnt = 0
-      max_value = 0
       
-      for i, v in enumerate(number):
-            print("cnt : %s " % cnt)
-            if cnt == k:
-                  print("append 수행")
-                  res.append(v)
-                  
-                  continue
-            print("i : %s " % i)
-            print("v : %s " % v)
-            if i == 0:
-                  print("첫번째")
-                  res.append(v)
-                  max_value = v
-                  
-                  print("res : %s " % res)
-            
-            elif i != 0 and res[-1] < v:
-                  if max_value < v:
-                        max_value = v
-                  
-                  for num in res:
-                        res.remove(num)
-                  
-                  print("pop수행")
-                  res.pop()
-                  cnt += 1
-                  
-                  res.append(v)
-                  print("res : %s " % res)
-            else:
-                  res.append(v)
-      
-      print("res : %s " % res)
-      
-      return res
+      number = list(number)
+      result = [number.pop(0)]
+      for n in number:
+            if result[-1] < n:
+                  while result and result[-1] < n and k > 0:
+                        result.pop()
+                        k -= 1
+                  result.append(n)
+            elif k == 0 or result[-1] >= n:
+                  result.append(n)
+      if k:
+            result = result[:-k]
+      answer = ''.join(result)
+      return answer
 
 
 
-# solution("1924", 2)
-# solution("1231234", 3)
+
+
+
+solution("1924", 2)
+solution("1231234", 3)
 solution("4177252841", 4)
