5acf86abcef5606ca3681f3a4e91e1f29344af2e
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-04-29 00:21:22 +0900

    파이썬 알고리즘 시작

--- JumpToPython/파이썬둘러보기/백준_그래프탐색/2178_미로탐색_S1.py
+++ JumpToPython/파이썬둘러보기/백준_그래프탐색/2178_미로탐색_S1.py
@@ -9,3 +9,28 @@
  * -----------------------------------------------------------
  * 2022-04-13        ipeac       최초 생성
  """
+from collections import deque
+
+def bfs(i, j):
+      queue.append((i, j))
+      visited[i][j] = 1
+      while queue:
+            x, y = queue.popleft()
+            for k in range(4):
+                  nx, ny = x + dx[k], y + dy[k]
+                  if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == 0 and graph[nx][ny] == 1:
+                        queue.append((nx, ny))
+                        visited[nx][ny] = visited[x][y] + 1
+
+
+N, M = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(N)]
+
+visited = [[0] * M for _ in range(N)]
+
+queue = deque()
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+bfs(0, 0)
+
+print(visited[-1][-1])
