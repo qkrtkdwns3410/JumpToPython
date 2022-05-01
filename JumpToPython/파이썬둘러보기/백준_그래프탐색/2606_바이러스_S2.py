a4f231ef09f2343e7fb23d48acbe2f17876c30b6
jihye94 <qkrtkdwns3410@naver.com>
2022-05-01 23:02:39 +0900

    p

--- JumpToPython/파이썬둘러보기/백준_그래프탐색/2606_바이러스_S2.py
+++ JumpToPython/파이썬둘러보기/백준_그래프탐색/2606_바이러스_S2.py
@@ -9,9 +9,28 @@
  * -----------------------------------------------------------
  * 2022-04-29        jihye94       최초 생성
  """
+from collections import deque
 
-def bfs():
-      pass
-
-# N  > 컴퓨터의 수
 N = int(input())
+visited = [0] * (N + 1)
+graph = [[] for _ in range(N + 1)]
+
+# 노드의 개수
+NN = int(input())
+for i in range(NN):
+      a, b = map(int, input().split())
+      graph[a].append(b)
+      graph[b].append(a)
+
+visited[1] = 1
+queue = deque([1])
+while queue:
+      x = queue.popleft()
+      for i in range(len(graph[x])):
+            
+            if visited[graph[x][i]] == 0:
+                  
+                  queue.append(graph[x][i])
+                  visited[graph[x][i]] = 1
+
+print(sum(visited) - 1)
