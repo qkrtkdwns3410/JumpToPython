a00b0b78fb1729c7a32c6ecd7f7fe0bdcf034bf3
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-06-09 00:39:41 +0900

    파이썬 알고리즘 시작

---
+++ JumpToPython/파이썬둘러보기/백준_그래프탐색/24479_알고리즘 수업_깊이우선탐색 1 _S2.py
@@ -0,0 +1,21 @@
+"""
+ *packageName    : 
+ * fileName       : 24479_알고리즘 수업_깊이우선탐색 1 _S1
+ * author         : ipeac
+ * date           : 2022-06-08
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-06-08        ipeac       최초 생성
+ """
+N, M, R = (map(int, input()))
+print("N : %s " % N)
+
+nodes = [[] for _ in range(N + 1)]
+print("nodes : %s " % nodes)
+
+visited = [[False] for _ in range(N + 1)]
+nodes_cnt = [0 for _ in range(N + 1)]
+for _ in range(M):
+      pass
