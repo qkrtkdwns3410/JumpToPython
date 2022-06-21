2f1a8a187bce4b10bd4d348e69e96f25b130b9ab
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-06-21 23:24:03 +0900

    파이썬 알고리즘 시작

---
+++ JumpToPython/파이썬둘러보기/백준_그래프탐색/6118_숨바꼭질_S1.py
@@ -0,0 +1,45 @@
+"""
+ *packageName    :
+ * fileName       : 6118_숨바꼭질_S1
+ * author         : ipeac
+ * date           : 2022-06-21
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-06-21        ipeac       최초 생성
+ """
+from collections import deque
+
+
+def bfs(R):
+      global visited
+      
+      q = deque()
+      q.append(R)
+      visited[R] = 1
+      while q:
+            x = q.popleft()
+            
+            for w in graph[x]:
+                  if not visited[w]:
+                        visited[w] = visited[x] + 1
+                        q.append(w)
+
+
+
+
+
+
+n, m, = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+
+for i in range(m):
+      A_i, B_i = map(int, input().split())
+      graph[A_i].append(B_i)
+      graph[B_i].append(A_i)
+
+bfs(1)
+max_value = max(visited)
+print(visited.index(max_value), max_value - 1, visited.count(max_value))
