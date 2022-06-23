360984f5189006b10d6ccaaed26a47de9dd1a7c3
qkrtkdwns3410 <80584060+qkrtkdwns3410@users.noreply.github.com>
2022-06-23 21:33:36 +0900

    [Silver I] Title: 돌다리, Time: 292 ms, Memory: 33496 KB -BaekjoonHub

---
+++ 백준/Silver/12761. 돌다리/돌다리.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+
+def bfs(graph, n):
+      visited = [0] * 100001
+      q = deque()
+      q.append(n)
+      visited[n] = 1
+      
+      while q:
+            x = q.popleft()
+            
+            for i in range(8):
+                  if i < 6:
+                        nx = x + dx[i]
+                  else:
+                        nx = x * dx[i]
+                  
+                  if 0 <= nx <= 100000 and visited[nx] == 0:
+                        graph[nx] = graph[x] + 1
+                        visited[nx] = 1
+                        q.append(nx)
+
+
+
+# 스카이콩콩 a | b 의 힘 , 동규와 주미의 현재 위치 n m
+a, b, n, m = map(int, input().split())
+
+dx = [1, -1, a, b, -a, -b, a, b]
+
+graph = [0] * 100001
+bfs(graph, n)
+print(graph[m])
