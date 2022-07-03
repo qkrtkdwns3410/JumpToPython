df242374a46cf0849e83f7e60135d08ce80250ac
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-07-03 16:28:39 +0900

    파이썬 알고리즘 시작

--- JumpToPython/파이썬둘러보기/백준_그래프탐색/16918_봄버맨_S1.py
+++ JumpToPython/파이썬둘러보기/백준_그래프탐색/16918_봄버맨_S1.py
@@ -9,3 +9,41 @@
  * -----------------------------------------------------------
  * 2022-07-03        ipeac       최초 생성
  """
+from collections import deque
+
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+q = deque()
+
+def bfs(graph, visited):
+    while q:
+        x, y = q.popleft()
+        
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx <= r and 0 <= ny <= c and not visited[nx][ny]:
+                graph[nx][ny] = '.'
+
+
+def simulate():
+    global time, graph
+    # 시간이 1초 인 경우
+    if time == 1:
+        pass
+    # 3번의 경우에는 짝수초
+    elif time % 2 == 0:
+        pass
+    # 4번의 경우 홀수 초인 경우
+    elif time % 2 == 1:
+        pass
+
+
+
+
+# r : 가로 줄 , c : 세로 줄 , n : n 초 후 격자판의 상태 출력
+r, c, n = map(int, input().split())
+
+graph = [list(map(str, input())) for _ in range(r)]
+print("graph : %s " % graph)
+visited = [[0] * c for _ in range(r)]
+print("visited : %s " % visited)
+time = 1
